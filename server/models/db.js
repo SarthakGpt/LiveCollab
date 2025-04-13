ca85546ba8b76b41993ff784602f5dbb04417425
unknown <guptasarthak2502@gmail.com>
2025-04-13 19:58:22 +0530

    index,db,package changed

--- server/models/db.js
+++ server/models/db.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config()
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_CONN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_CONN
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+        );
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('MONGODB connection Error:', error);
